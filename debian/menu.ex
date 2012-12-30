5206457f71887041debe5a8eaf75e13ff58904ec
Devon Kearns <dookie@kali.org>
2012-12-30 16:39:44 -0700

    Pre-build commit

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(rtpinsertsound):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="rtpinsertsound" command="/usr/bin/rtpinsertsound"
--- debian/rtpinsertsound.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the rtpinsertsound package
-#
-0 4	* * *	root	[ -x /usr/bin/rtpinsertsound_maintenance ] && /usr/bin/rtpinsertsound_maintenance
